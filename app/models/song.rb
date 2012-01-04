ff54e1906a8d47bc06f6bdbf9de743759450b922
Thorsten Ball <mrnugget@gmail.com>
2012-01-04 13:46:46 +0100

    Refactor Song.has_tag? and validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,10 +13,12 @@ class Song < ActiveRecord::Base
   belongs_to :session
 
   has_many :song_tags
-  has_many :tags, :through => :song_tags
+  has_many :tags, through: :song_tags
 
-  validates :file_name, :presence => true
-  validate :file_has_to_be_audio
+  validates :file_name, presence: true, format: {
+    with: %r{\.(mp3|wav|flac)$}i,
+    message: 'must be a mp3/wav/flac file.'
+  }
 
   def add_tag(new_tag_name)
     tag = Tag.find_or_create_by_name(name: new_tag_name)
@@ -25,15 +27,6 @@ class Song < ActiveRecord::Base
 
   def has_tag?(tag_name)
     tag = Tag.find_by_name(tag_name)
-    return nil  if tag.nil?
-    return true if song_tags.find_by_tag_id_and_song_id(tag.id, id)
+    tag.present? && song_tags.find_by_tag_id_and_song_id(tag.id, id).present?
   end
-
-  private
-
-    def file_has_to_be_audio
-      unless ['mp3', 'flac','wav'].include? file_name.split(".").last.downcase
-        errors.add(:file_name, "has to be mp3/flac/wav file")
-      end
-    end
 end

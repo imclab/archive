f1c67ab6e7b40f11321e4d9d366d98289295424b
Thorsten Ball <mrnugget@gmail.com>
2012-02-04 13:12:03 +0100

    Updating Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,21 +5,21 @@ set :user, "mrnugget"
 set :domain, "howlingvibes.com"
 
 set :repository,  "git@github.com:mrnugget/archive.git"
-set :deploy_to, "/home/#{user}/rails_apps"
+set :deploy_to, "/home/#{user}/rails_apps/#{application}"
 default_run_options[:pty] = true 
 
 set :scm, 'git'
 set :branch, 'master'
 set :scm_verbose, true
 set :deploy_via, :remote_cache
-set :use_sudo, true
+set :use_sudo, false
 set :rails_env, :production
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, domain
 role :app, domain
-role :db, domain 
+role :db, domain, :primary => true
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -29,6 +29,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
+    run '#{try_sudo) rm /var/www/howlingvibes.com/htdocs/archive'
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end

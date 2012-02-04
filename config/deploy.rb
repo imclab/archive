2706481ebdca6fea75b09a691dfd4e8f19cb6497
Thorsten Ball <mrnugget@gmail.com>
2012-02-04 13:39:06 +0100

    Bugfix: No delete link for non-users

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,6 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run '#{try_sudo) rm /var/www/howlingvibes.com/htdocs/archive'
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end

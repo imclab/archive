8b1896d2d58f3607b4a0cab195819cb0ec46eca6
Thorsten Ball <mrnugget@gmail.com>
2012-03-24 14:00:40 +0100

    Sorting now works for Sessions and Songs

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -9,6 +9,28 @@ describe "Sessions" do
       page.should have_css('title',
                            :text => 'Howling Vibes Archive | All sessions')
     end
+
+    describe "clicking on the 'Show' links" do
+      before(:each) do
+        @old_session = Session.create!(session_date: Time.now - 2.days)
+        @old_song = @old_session.songs.create!(file_name: "01.breaking.bad.mp3") 
+        
+        @new_session = Session.create!(session_date: Time.now)
+        @new_song = @new_session.songs.create!(file_name: "03.hello_joe.mp3")
+      end
+
+      it "should show the oldest session on top when clicked on 'Oldest'" do
+        visit '/sessions/'
+        click_link "Oldest"
+        page.body.should =~ /#{@old_song.file_name}.*#{@new_song.file_name}/m
+      end
+
+      it "should show the newest session on top when clicked on 'Newest'" do
+        visit '/sessions/'
+        click_link "Newest"
+        page.body.should =~ /#{@new_song.file_name}.*#{@old_song.file_name}/m
+      end
+    end
   end
 
   describe "GET 'new' as non-user" do
--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -3,16 +3,45 @@ require 'spec_helper'
 describe "Songs" do
 
   describe "GET 'index'" do
-    before(:each) do
-      @song = Song.create!(file_name: "01.testing.mp3")
-    end
     it "should show a songs list" do
+      @song = Song.create!(file_name: "01.testing.mp3")
       visit '/songs'
       current_path.should == songs_path
       page.should have_css("title",
                            :title => "Howling Vibes Archive | All songs")
       page.should have_css("ul.song-list")
       page.should have_link(@song.file_name, :href => song_path(@song))
+    end
+
+    describe "clicking on the 'Show' links" do
+      before(:each) do
+        @old_session = Session.create!(session_date: Time.now - 2.days)
+        @old_song = @old_session.songs.create!(file_name: "01.breaking.bad.mp3") 
+        
+        @new_session = Session.create!(session_date: Time.now)
+        @new_song = @new_session.songs.create!(file_name: "03.hello_joe.mp3")
+      end
+
+      it "should show the oldest songs when clicked on 'Oldest'" do
+        visit '/songs'
+        click_link "Oldest"
+        page.body.should =~ /#{@old_song.file_name}.*#{@new_song.file_name}/m
+      end
+
+      it "should show the newest songs on top when clicked on 'Newest'" do
+        visit '/songs/'
+        click_link "Newest"
+        page.body.should =~ /#{@new_song.file_name}.*#{@old_song.file_name}/m
+      end
+
+      it "should show the most tagged song on top when clicked on 'most tagged'" do
+        @old_song.add_tag("hello there")
+        @old_song.add_tag("and you")
+        @new_song.add_tag("one tag")
+        visit '/songs/'
+        click_link "Most Tagged"
+        page.body.should =~ /#{@old_song.file_name}.*#{@new_song.file_name}/m
+      end
     end
   end
 

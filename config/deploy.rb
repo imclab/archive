9e0ca0087e0370658a780f3688676c7fb9c52eeb
Thorsten Ball <mrnugget@gmail.com>
2012-06-13 19:20:34 +0200

    Update to Rails 3.2.6

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,8 +42,8 @@ end
   
 after "deploy:finalize_update", "db:copy_db_config"
 
-# after "deploy:update_code", :bundle_install
-# desc "install the necessary prerequisites"
-# task :bundle_install, :roles => :app do
-#   run "cd #{release_path} && bundle install --deployment"
-# end
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install --deployment"
+end

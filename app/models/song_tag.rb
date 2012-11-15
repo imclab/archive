c6a3604965c1e362d31aaeaf140c26fb6eac3c38
Thorsten Ball <mrnugget@gmail.com>
2012-11-15 19:59:18 +0100

    Clean up songtag model and songtag spec

--- app/models/song_tag.rb
+++ app/models/song_tag.rb
@@ -1,6 +1,6 @@
 class SongTag < ActiveRecord::Base
-  validates :song_id, :presence => true
-  validates :tag_id,  :presence => true
+  validates :song_id, presence: true
+  validates :tag_id,  presence: true
 
   belongs_to :song
   belongs_to :tag
--- spec/models/song_tag_spec.rb
+++ spec/models/song_tag_spec.rb
@@ -1,23 +1,13 @@
 require 'spec_helper'
 
 describe SongTag do
-
-  describe "validations" do
-
-    before(:each) do
-      @song     = Song.create!(file_name: "01.testing.mp3")
-      @tag      = Tag.create!(name: "great")
-      @song_tag = SongTag.new(tag: @tag, song: @song)
+  describe 'validations' do
+    it 'should require a song_id' do
+      SongTag.new(song_id: nil).should have(1).error_on(:song_id)
     end
 
-    it "should require a song_id" do
-      @song_tag.song = nil
-      @song_tag.should_not be_valid
-    end
-
-    it "should require a tag_id" do
-      @song_tag.tag = nil
-      @song_tag.should_not be_valid
+    it 'should require a tag_id' do
+      SongTag.new(tag_id: nil).should have(1).error_on(:tag_id)
     end
   end
 end

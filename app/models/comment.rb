6e5ce43b97a2f4bfad3177ed1512802e448b469b
Thorsten Ball <mrnugget@gmail.com>
2012-11-15 19:55:31 +0100

    Clean up comment model and comment spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
-  validates :text, presence: true,
-                   length: { maximum: 500 }
+  validates :text, presence: true, length: { maximum: 500 }
   belongs_to :user
   belongs_to :song
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,30 +1,27 @@
 require 'spec_helper'
 
 describe Comment do
-  describe "validation" do
-    it "should not be valid with blank text" do
-      invalid_comment = Comment.new(text: "")
-      invalid_comment.should_not be_valid
-      invalid_comment.should have(1).error_on(:text)
+  describe 'validation' do
+    it 'should not be valid with blank text' do
+      Comment.new(text: '').should have(1).error_on(:text)
     end
 
-    it "should not be valid with too much text" do
-      long_text = "a" * 501
-      invalid_comment = Comment.new(text: long_text)
-      invalid_comment.should_not be_valid
+    it 'should not be valid with too much text' do
+      Comment.new(text: 'a' * 501).should_not be_valid
     end
   end
 
-  describe "associations with song and user models" do
-    it "should respond with the right models" do
-      song = Song.create!(file_name: "01.testing.mp3")
+  describe 'associations with song and user models' do
+    it 'should respond with the right models' do
+      song = Song.create!(file_name: '01.testing.mp3')
       user = User.create!(
-        name: "The Stig",
-        email: "stig@topgear.co.uk",
-        password: "foobar",
-        password_confirmation: "foobar"
+        name: 'The Stig',
+        email: 'stig@topgear.co.uk',
+        password: 'foobar',
+        password_confirmation: 'foobar'
       )
-      comment = Comment.create!(song: song, user: user, text: "this rocks")
+
+      comment = Comment.create!(song: song, user: user, text: 'this rocks')
 
       comment.song.should == song
       comment.user.should == user

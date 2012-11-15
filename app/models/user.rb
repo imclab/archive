e94e977335a580bc0717c105a5e154013712e166
Thorsten Ball <mrnugget@gmail.com>
2012-11-15 19:22:30 +0100

    Clean up user model and user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,15 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  validates :name, :presence => true,
-                   :length   => { :maximum => 30 }
+  validates :name, presence: true,
+                   length: { maximum: 30 }
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, :presence   => true,
-                    :format     => { :with => email_regex},
-                    :uniqueness => { :case_sensitive => false }
+  validates :email, format: { with: email_regex},
+                    uniqueness: { case_sensitive: false }
 
-  validates :password, :presence => { :on  => :create },
-                       :length   => { :within => 6..40 }
+  validates :password, presence: { on: :create },
+                       length: { within: 6..40 }
 
   has_many :comments
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,93 +1,71 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @attr = {
-      :name                  => "The Stig",
-      :email                 => "stig@topgear.co.uk",
-      :password              => "foobar",
-      :password_confirmation => "foobar"
+  let(:attributes) do
+    {
+      name: 'The Stig',
+      email: 'stig@topgear.co.uk',
+      password: 'foobar',
+      password_confirmation: 'foobar'
     }
   end
 
-  it "should require a name" do
-    User.new(@attr.merge(:name => "")).should_not be_valid
+  it { should have(1).error_on(:name) }
+  it { should have(1).error_on(:email) }
+
+  it 'rejects names that are too long' do
+    User.new(attributes.merge(name: 'a' * 31)).should_not be_valid
   end
 
-  it "should require an email adress" do
-    User.new(@attr.merge(:email => "")).should_not be_valid
-  end
-
-  it "should reject names that are too long" do
-    User.new(@attr.merge(name: 'a' * 31)).should_not be_valid
-  end
-
-  it "should accept valid email adresses" do
+  it 'should accept valid email adresses' do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jpg]
     addresses.each do |address|
-      user = User.new(@attr.merge(email: address))
+      user = User.new(attributes.merge(email: address))
       user.should be_valid
     end
   end
 
-  it "should reject invalid email adresses" do
+  it 'should reject invalid email adresses' do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
-      user = User.new(@attr.merge(:email => address))
+      user = User.new(attributes.merge(:email => address))
       user.should_not be_valid
     end
   end
 
-  it "should reject duplicate email addresses" do
-    User.create!(@attr)
+  it 'should reject duplicate email addresses' do
+    User.create!(attributes)
 
-    user = User.new(@attr)
+    user = User.new(attributes)
     user.should have(1).error_on(:email)
   end
 
-  it "should reject email addresses identical up to case" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(email: upcased_email))
+  it 'should reject email addresses identical up to case' do
+    User.create!(attributes.merge(email: attributes[:email].upcase))
 
-    user = User.new(@attr)
+    user = User.new(attributes)
     user.should have(1).error_on(:email)
   end
 
-  describe "password validations" do
-    it "should require a password" do
-      User.new(@attr.merge(password: '', password_confirmation: '')).should_not be_valid
-    end
-
-    it "should require a matching password confirmation" do
-      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
-    end
-
-    it "should reject short passwords" do
-      User.new(@attr.merge(password: 'a' * 5, password_confirmation: 'a' * 5)).should_not be_valid
-    end
-
-    it "should reject long passwords" do
-      User.new(@attr.merge(password: 'a' * 41, password_confirmation: 'a' * 41)).should_not be_valid
-    end
+  it 'should not be an admin by default' do
+    User.new.should_not be_admin
   end
 
-  describe "admin attribute" do
-    before(:each) do
-      @user = User.create!(@attr)
+  describe 'password validations' do
+    it 'should require a password' do
+      User.new(attributes.merge(password: '', password_confirmation: '')).should_not be_valid
     end
 
-    it "should not be an admin by default" do
-      @user.should_not be_admin
+    it 'should require a matching password confirmation' do
+      User.new(attributes.merge(:password_confirmation => "invalid")).should_not be_valid
     end
-  end
 
-  describe "association with comments" do
-    it "should show the right comments" do
-      user        = User.create!(@attr)
-      comment_one = Comment.create!(user_id: user.id, text: "Stig was here!")
-      comment_two = Comment.create!(user_id: user.id, text: "Here too")
+    it 'should reject short passwords' do
+      User.new(attributes.merge(password: 'a' * 5, password_confirmation: 'a' * 5)).should_not be_valid
+    end
 
-      user.comments.should == [comment_one, comment_two]
+    it 'should reject long passwords' do
+      User.new(attributes.merge(password: 'a' * 41, password_confirmation: 'a' * 41)).should_not be_valid
     end
   end
 end

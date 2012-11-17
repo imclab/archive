a8f650590c8d802bbd5176894b379cb16169fef6
Thorsten Ball <mrnugget@gmail.com>
2012-11-17 18:07:38 +0100

    Change formatting in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,15 +33,15 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.clean_with(:truncation)
-    end
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end

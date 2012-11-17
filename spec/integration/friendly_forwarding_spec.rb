54e62206c14c93bcb7643eb68de6b8f59bb0e3e7
Thorsten Ball <mrnugget@gmail.com>
2012-11-17 10:22:14 +0100

    Clean up friendly_forwarding spec

--- spec/integration/friendly_forwarding_spec.rb
+++ spec/integration/friendly_forwarding_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
-describe 'Friendly Forwarding' do
+describe 'friendly forwarding' do
+  let(:session) {
+    Session.create!(session_date: Time.now)
+  }
+
   before(:each) do
     create_user
-    session = Session.create!(session_date: Time.now)
-    @song   = session.songs.create!(file_name: "01.testing.mp3")
+    @song = session.songs.create!(file_name: "01.testing.mp3")
   end
 
-  it "redirects user to previous path after login" do
+  it 'redirects user to previous path after login' do
     visit song_path(@song)
+
     integration_sign_in
 
-    page.should have_content("01.testing.mp3")
     current_path.should == song_path(@song)
   end
 end

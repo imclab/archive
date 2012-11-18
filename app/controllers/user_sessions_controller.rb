b7c2424e185092229f839bf73641482968d37d18
Thorsten Ball <mrnugget@gmail.com>
2012-11-18 13:47:55 +0100

    Clean up UserSessions controller and spec

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,19 +4,20 @@ class UserSessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:user_session][:email])
+
     if user && user.authenticate(params[:user_session][:password])
-      sign_in user
+      sign_in(user)
       flash_message :success, "Hello #{user.name}, you successfully logged in!"
       redirect_to session.delete(:return_to) || sessions_path
     else
-      flash_message :error, "Invalid email or password"
-      render "new"
+      flash_message :error, 'Invalid email or password'
+      render 'new'
     end
   end
 
   def destroy
     sign_out
-    flash_message :notification, "You successfully logged out."
+    flash_message :notification, 'You successfully logged out.'
     redirect_to sessions_path
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,86 +1,58 @@
 require 'spec_helper'
 
 describe UserSessionsController do
+  describe 'create' do
+    context "invalid login data" do
+      it 'should re-render the new page' do
+        post :create, user_session: { email: 'email@example.com', password: 'invalid' }
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get :new
-      response.should be_success
-    end
-  end
-
-  describe "GET '/signin'" do
-    it "should be successful" do
-      visit '/signin'
-      response.should be_success
-    end
-  end
-
-  describe "POST 'create'" do
-    describe "invalid signin" do
-
-      before(:each) do
-        @attr = { :email => "email@example.com", :password => "invalid" }
-      end
-
-      it "should re-render the new page" do
-        post :create, :user_session => @attr
         response.should render_template('new')
       end
     end
 
-    describe "valid signin with valid email and password" do
-     before(:each) do
-        @user = User.create!(:name => "John Doe",
-                     :email => "john@doe.org",
-                     :password => "password",
-                     :password_confirmation => "password")
-        @attr = { :email => @user.email, :password => @user.password }
+    context 'valid login data' do
+      let(:user) {
+        create_user
+      }
+
+      it 'should sign the user in' do
+        post :create, user_session: { email: user.email, password: user.password }
+
+        controller.current_user.should == user
       end
 
-      it "should sign the user in" do
-        post :create, :user_session => @attr
-        controller.current_user.should == @user
-        controller.should be_signed_in
-      end
+      it 'redirects to the sessions page' do
+        post :create, user_session: { email: user.email, password: user.password }
 
-      it "should redirect to the sessions page" do
-        post :create, :user_session => @attr
         response.should redirect_to(sessions_path)
       end
 
 
-      it "redirects to return_to path" do
+      it 'redirects to return_to path' do
         session[:return_to] = '/bananensaft'
 
-        post :create, :user_session => @attr
+        post :create, user_session: { email: user.email, password: user.password }
+
         response.should redirect_to('/bananensaft')
       end
 
-      it "clears return_to path after redirect" do
+      it 'clears return_to path after redirect' do
         session[:return_to] = '/bananensaft'
 
-        post :create, :user_session => @attr
+        post :create, user_session: { email: user.email, password: user.password }
+
         session[:return_to].should be_nil
       end
     end
   end
 
-  describe "DELETE 'destroy'" do
-    before(:each) do
-      @user = User.create!(:name => "John Doe",
-                   :email => "john@doe.org",
-                   :password => "password",
-                   :password_confirmation => "password")
-      @attr = { :email => @user.email, :password => @user.password }
-      post :create, :user_session => @attr
-    end
+  describe 'destroy' do
+    it 'should sign user out' do
+      controller_sign_in(create_user)
 
-    it "should sign user out" do
       delete :destroy
-      controller.should_not be_signed_in
+
       controller.current_user.should == nil
-      response.should redirect_to(sessions_path)
     end
   end
 end

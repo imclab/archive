5d4904efdc577dc7768ea7540260b00cae3dd5a6
Thorsten Ball <mrnugget@gmail.com>
2012-12-25 10:28:02 +0100

    Remove useless link and before_filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authorize,    only: [:edit, :update, :index]
+  before_filter :authorize,    only: [:edit, :update]
   before_filter :correct_user, only: [:edit, :update]
 
   def new

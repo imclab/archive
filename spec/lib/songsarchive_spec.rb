f3a876f6b795b2ca97ea2d08a9aeafdd89dc1a97
Thorsten Ball <mrnugget@gmail.com>
2013-04-16 09:18:50 +0200

    Clean up SongsArchive spec

--- spec/lib/songsarchive_spec.rb
+++ spec/lib/songsarchive_spec.rb
@@ -1,24 +1,27 @@
 require File.expand_path('../../../app/lib/songs_archive', __FILE__)
 
 describe SongsArchive::Directory do
-  before(:each) do
-    new_dir  = File.expand_path('../../fixtures/archive', __FILE__)
-    @archive = SongsArchive::Directory.new(new_dir)
+  let(:fixtures) do
+    File.expand_path('../../fixtures/archive', __FILE__)
+  end
+
+  let(:archive) do
+    SongsArchive::Directory.new(fixtures)
   end
 
   it "should return a list of sessions" do
-    @archive.sessions.should include('2011.07.01', '2011.07.07', '2011.08.02')
+    archive.sessions.should include('2011.07.01', '2011.07.07', '2011.08.02')
   end
 
   it "should return a list of sessions, and only sessions" do
-    @archive.sessions.should_not include('Brezel', 'Superman', 'Sanchez')
+    archive.sessions.should_not include('Brezel', 'Superman', 'Sanchez')
   end
 
   it "should return a list of files for a session" do
-    @archive.files_in_session('2011.07.21').should include('02.you_too.mp3')
+    archive.files_in_session('2011.07.21').should include('02.you_too.mp3')
   end
 
   it "should only return audio files as session files" do
-    @archive.files_in_session('2011.07.21').should_not include('README.md', 'lolzomat')
+    archive.files_in_session('2011.07.21').should_not include('README.md', 'lolzomat')
   end
 end

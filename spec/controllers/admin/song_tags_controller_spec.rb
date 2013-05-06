de487aa5962f0ae87bd1b468bd9dacdc5d4923fe
Thorsten Ball <mrnugget@gmail.com>
2013-05-06 21:36:40 +0200

    Clean up Admin::SongTags spec

--- spec/controllers/admin/song_tags_controller_spec.rb
+++ spec/controllers/admin/song_tags_controller_spec.rb
@@ -2,56 +2,53 @@ require 'spec_helper'
 
 describe Admin::SongTagsController do
   describe "#destroy" do
-    let(:song1) do
+    let(:song) do
       Song.create!(file_name: '01.testing.mp3')
     end
 
-    let(:song2) do
-      Song.create!(file_name: '02.testing.mp3')
-    end
-
-    let(:tag1) do
-      song1.tags.create!(name: 'super')
-    end
-
-    let(:tag2) do
-      song1.tags.create!(name: 'awesome')
+    let(:tag) do
+      song.tags.create!(name: 'super')
     end
 
     before(:each) do
       admin = create_user
       admin.toggle!(:admin)
       controller_sign_in(admin)
-
-      song2.tags << tag1
     end
 
     it 'deletes the relationship between a song and a tag' do
-      song_tag = SongTag.where(song_id: song1.id, tag_id: tag1.id).first
+      song_tag = SongTag.where(song_id: song.id, tag_id: tag.id).first
+
       lambda do
         delete :destroy, id: song_tag.id
       end.should change(SongTag, :count).by(-1)
     end
 
-    it 'redirects to the songs show action' do
-      song_tag = SongTag.where(song_id: song1.id, tag_id: tag1.id).first
-      delete :destroy, id: song_tag.id
-
-      response.should redirect_to(admin_song_path(song1.id))
-    end
-
     it 'does not delete tags if they are associated with other songs' do
-      song_tag = SongTag.where(song_id: song1.id, tag_id: tag1.id).first
+      song2 = Song.create!(file_name: '02.testing.mp3')
+      song2.tags << tag
+
+      song_tag = SongTag.where(song_id: song.id, tag_id: tag.id).first
+
       lambda do
         delete :destroy, id: song_tag.id
       end.should_not change(Tag, :count).by(-1)
     end
 
     it 'deletes tags if they are not associated with other songs' do
-      song_tag = SongTag.where(song_id: song1.id, tag_id: tag2.id).first
+      song_tag = SongTag.where(song_id: song.id, tag_id: tag.id).first
+
       lambda do
         delete :destroy, id: song_tag.id
       end.should change(Tag, :count).by(-1)
+    end
+
+    it 'redirects to the songs show action' do
+      song_tag = SongTag.where(song_id: song.id, tag_id: tag.id).first
+
+      delete :destroy, id: song_tag.id
+
+      response.should redirect_to(admin_song_path(song.id))
     end
   end
 end

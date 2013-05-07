7e898ea1e7b64684f264fefaffe7c3a09270e823
Thorsten Ball <mrnugget@gmail.com>
2013-05-07 07:39:22 +0200

    Refactor SongsController spec

--- spec/controllers/admin/songs_controller_spec.rb
+++ spec/controllers/admin/songs_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe Admin::SongsController do
+  let(:song) do
+    Song.create!(file_name: '01.testing.mp3')
+  end 
+
   before(:each) do
     admin = create_user
     admin.toggle!(:admin)
     controller_sign_in(admin)
-    session = Session.create!(session_date: Time.now)
-    @song = session.songs.create!(file_name: '01.testing.mp3')
   end
 
   describe 'show' do
@@ -14,16 +16,18 @@ describe Admin::SongsController do
       songs_stub = stub(:songs_with_include)
 
       Song.should_receive(:includes).and_return(songs_stub)
-      songs_stub.should_receive(:find).with(@song.id.to_s)
+      songs_stub.should_receive(:find).with(song.id.to_s)
 
-      get :show, id: @song.id
+      get :show, id: song.id
     end
   end
 
   describe 'destroy' do
+    before { song.touch }
+
     it 'deletes a song' do
       lambda do
-        delete :destroy, id: @song.id
+        delete :destroy, id: song.id
       end.should change(Song, :count).by(-1)
     end
   end
